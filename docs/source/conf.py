9e81f07dd11be1cff06f4024013efdbd6da1e2e8
Alexander Tritt (Monash) <alexander.tritt@monash.edu>
2020-11-30 16:12:44 +1100

    Correct dependencies in toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,7 +39,7 @@ copyright = '2020, Monash University'
 author = 'Alexander Tritt'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.3'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
